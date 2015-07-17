09ae413550520e7af095ab44073fc44a2435b95e
Tarun Arora <tananrules@gmail.com>
2015-07-17 15:12:19 +0530

    posts firebase test

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'firebase-test',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://posts-test.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
